61f570c0277ca898ea505e599496cf9f46d81b1e
parzip4l <sobirinjamo33@gmail.com>
2023-10-08 15:53:28 +0700

    tinggaleun

--- app/Http/Controllers/Absen/AbsenController.php
+++ app/Http/Controllers/Absen/AbsenController.php
@@ -103,7 +103,7 @@ class AbsenController extends Controller
 
     private function calculateDistance($lat1, $lon1, $lat2, $lon2)
     {
-        $earthRadius = 6371; // radius Bumi dalam kilometer
+        $earthRadius = 6371; 
 
         $dLat = deg2rad($lat2 - $lat1);
         $dLon = deg2rad($lon2 - $lon1);
@@ -111,7 +111,7 @@ class AbsenController extends Controller
         $a = sin($dLat / 2) * sin($dLat / 2) + cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * sin($dLon / 2) * sin($dLon / 2);
         $c = 2 * atan2(sqrt($a), sqrt(1 - $a));
 
-        $distance = $earthRadius * $c; // jarak dalam kilometer
+        $distance = $earthRadius * $c; 
 
         return $distance;
     }

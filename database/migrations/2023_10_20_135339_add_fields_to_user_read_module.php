b7327a4c179edb243978d8e05480624016b03bfa
parzip4l <sobirinjamo33@gmail.com>
2023-10-20 14:55:02 +0700

    update

--- database/migrations/2023_10_20_135339_add_fields_to_user_read_module.php
+++ database/migrations/2023_10_20_135339_add_fields_to_user_read_module.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::connection('mysql_secondary')->table('user_read_module', function (Blueprint $table) {
+        Schema::connection('mysql_secondary')->table('user_read_modules', function (Blueprint $table) {
             $table->string('employee_code');
             $table->string('id_module');
         });

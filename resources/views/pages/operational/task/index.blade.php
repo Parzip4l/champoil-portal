a0f1afa69fb7f88cde487a13f680e266deee046b
rinable <sobirin@champoil.co.id>
2023-11-13 11:58:48 +0700

    Update

--- resources/views/pages/operational/task/index.blade.php
+++ resources/views/pages/operational/task/index.blade.php
@@ -84,8 +84,6 @@
                             @endforeach
                         </tbody>
                     </table>
-                    
-
                 </div>
             </div>
         </div>
@@ -112,8 +110,9 @@
                             <label for="" class="form-label">Project</label>
                             <select name="project_id" id="project_id" class="form-control">
                                 <option value="">PILIH PROJECT</option>
+                                
                                 @foreach($project as $row)
-                                <option value="{{ $row->id }}">{{ $row->project }}</option>
+                                <option value="{{ $row->id }}">{{ $row->name }}</option>
                                 @endforeach
                             </select>
                         </div>

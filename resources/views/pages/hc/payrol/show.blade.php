a8325a153b70b015979c0550b25e080d54637447
rinable <sobirin@champoil.co.id>
2024-01-22 20:03:29 +0700

    Update Data

--- resources/views/pages/hc/payrol/show.blade.php
+++ resources/views/pages/hc/payrol/show.blade.php
@@ -71,10 +71,7 @@
                             <input type="number" id="t_alatkerja" class="form-control allowance" name="allowances[t_alatkerja][]" placeholder="Rp. " required value="{{ number_format($allowances['t_alatkerja'][0], 0, ',', '.') }}">
                         </div>
                         @endif
-                        <div class="col-md-6 mb-3">
-                            <label for="kode_karyawan" class="form-label">Total Allowance</label>
-                            <input type="number" id="t_allowance" class="form-control" name="allowances[t_allowance][]" placeholder="Rp. " required readonly value="{{ number_format($allowances['t_allowance'][0], 0, ',', '.') }}">
-                        </div>
+                        
                     </div>
                     @if($karyawanLogin->unit_bisnis === 'Kas')
                     <div class="row mb-3">
@@ -93,6 +90,10 @@
                         <div class="col-md-6 mb-3">
                             <label for="kode_karyawan" class="form-label">Gaji Rapel</label>
                             <input type="number" id="t_makan" class="form-control allowance" name="allowances[g_rapel][]" placeholder="Rp. " required>
+                        </div>
+                        <div class="col-md-6 mb-3">
+                            <label for="kode_karyawan" class="form-label">Total Allowance</label>
+                            <input type="number" id="t_allowance" class="form-control" name="allowances[t_allowance][]" placeholder="Rp. " required readonly value="{{ number_format($allowances['t_allowance'][0], 0, ',', '.') }}">
                         </div>
                     </div>
                     @endif

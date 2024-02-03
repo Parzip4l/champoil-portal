7e6973eb74eee315243771f0f8b6b68dde046495
rinable <sobirin@champoil.co.id>
2024-02-03 18:26:48 +0700

    tinggalen

--- app/Imports/EmployeeImport.php
+++ app/Imports/EmployeeImport.php
@@ -7,6 +7,7 @@ use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use App\Employee;
 use App\User;
 
@@ -19,57 +20,48 @@ class EmployeeImport implements ToCollection, WithHeadingRow
     {
         $code = Auth::user()->employee_code;
         $company = Employee::where('nik', $code)->first();
-        
-        try {
-            // Start a database transaction
-            DB::beginTransaction();
+        foreach ($rows as $row) {
+            $unit_bisnis = $company->unit_bisnis;
+            $useraccess = ["dashboard_access"];
+            $hashedPassword = Hash::make($row['password']);
 
-            foreach ($rows as $row) {
-                $unit_bisnis = $company->unit_bisnis;
-                $useraccess = ["dashboard_access"];
-                $hashedPassword = Hash::make($row['password']);
+            Employee::create([
+                'nama' => $row['nama'],
+                'ktp' => $row['ktp'],
+                'nik' => $row['nik'],
+                'referal_code' => $row['referal_code'],
+                'divisi' => $row['divisi'],
+                'jabatan' => $row['jabatan'],
+                'agama' => $row['agama'],
+                'jenis_kelamin' => $row['jenis_kelamin'],
+                'email' => $row['email'],
+                'telepon' => $row['telepon'],
+                'telepon_darurat' => $row['telepon_darurat'],
+                'status_kontrak' => $row['status_kontrak'],
+                'organisasi' => $row['organisasi'],
+                'joindate' => $row['joindate'],
+                'berakhirkontrak' => $row['berakhirkontrak'],
+                'tempat_lahir' => $row['tempat_lahir'],
+                'tanggal_lahir' => $row['tanggal_lahir'],
+                'alamat' => $row['alamat'],
+                'alamat_ktp' => $row['alamat_ktp'],
+                'status_pernikahan' => $row['status_pernikahan'],
+                'tanggungan' => $row['tanggungan'],
+                'pendidikan_trakhir' => $row['pendidikan_trakhir'],
+                'jurusan' => $row['jurusan'],
+                'unit_bisnis' => $unit_bisnis,
+                'sertifikasi' => $row['sertifikasi'],
+                'expired_sertifikasi' => $row['expired_sertifikasi'],
+            ]);
 
-                // Create Employee
-                $employee = Employee::create([
-                    'nama' => $row['nama'],
-                    'ktp' => $row['ktp'],
-                    'nik' => $row['nik'],
-                    'referal_code' => $row['referal_code'],
-                    'divisi' => $row['divisi'],
-                    'jabatan' => $row['jabatan'],
-                    'agama' => $row['agama'],
-                    'jenis_kelamin' => $row['jenis_kelamin'],
-                    'email' => $row['email'],
-                    // ... (other columns)
-                    'expired_sertifikasi' => $row['expired_sertifikasi'],
-                ]);
-
-                // Create User with hashed password and encoded permissions
-                $user = User::create([
-                    'name' => $row['nik'],
-                    'email' => $row['email'],
-                    'password' => $hashedPassword,
-                    'permission' => $useraccess,
-                    'employee_code' => $row['nik'],
-                    'company' => $unit_bisnis,
-                ]);
-
-                // If both operations are successful, you can perform additional actions here.
-                // Example: Send a notification, log the success, etc.
-            }
-
-            // Commit the transaction if all operations are successful
-            DB::commit();
-
-        } catch (\Exception $e) {
-            // Handle any exceptions or errors that may occur during the creation.
-            // You can log the error, send a notification, or perform any other necessary action.
-            
-            // Optionally, you can roll back the database transaction if needed.
-            DB::rollBack();
-
-            // Rethrow the exception to let it propagate further if needed.
-            throw $e;
+            User::create([
+                'name' => $row['nik'],
+                'email' => $row['email'],
+                'password' => $hashedPassword,
+                'permission' => $useraccess,
+                'employee_code' => $row['nik'],
+                'company' => $unit_bisnis,
+            ]);        
         }
     }
 }

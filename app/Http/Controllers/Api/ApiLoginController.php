1ddd351b98bab56c878d103d7d918c7fceadc063
rinable <sobirin@champoil.co.id>
2024-02-20 10:22:40 +0700

    Update Clockout

--- app/Http/Controllers/Api/ApiLoginController.php
+++ app/Http/Controllers/Api/ApiLoginController.php
@@ -171,9 +171,43 @@ class ApiLoginController extends Controller
             $token = $request->bearerToken();
             $user = Auth::guard('api')->user();
             $nik = $user->employee_code;
+            $unit_bisnis = Employee::where('nik',$nik)->first();
+
             $lat2 = $request->input('latitude_out');
             $long2 = $request->input('longitude_out');
+
+            $yesterday = Carbon::yesterday();
             $currentDate = now()->format('Y-m-d');
+            if (strcasecmp($unit_bisnis->unit_bisnis, 'Kas') == 0 && strcasecmp($unit_bisnis->organisasi, 'FRONTLINE OFFICER') == 0) {
+            
+                $scheduleKasYesterday = Schedule::where('employee', $nik)
+                    ->whereDate('tanggal', $yesterday)
+                    ->first();
+
+                if (strcasecmp($scheduleKasYesterday->shift, 'ML') == 0 ){
+                    $absensi = Absen::where('nik', $nik)
+                                ->whereDate('tanggal', $yesterday)
+                                ->orderBy('clock_in', 'desc')
+                                ->first();
+
+                    if ($absensi) {
+                        $absensi->clock_out = now()->format('H:i');
+                        $absensi->latitude_out = $lat2;
+                        $absensi->longtitude_out = $long2;
+                        $absensi->save();
+        
+                        return response()->json([
+                            'status' => 'success',
+                            'message' => 'Clockout success! Selamat Beristirahat!',
+                        ], 200);
+                    } else {
+                        return response()->json([
+                            'status' => 'error',
+                            'message' => 'No clock-in record found for Yesterday.',
+                        ], 404);
+                    }
+                }
+            }
 
             $absensi = Absen::where('nik', $nik)
                 ->whereDate('tanggal', $currentDate)

522b29bd5b421398b86cd3cff07a7a618a1423a7
rinable <sobirin@champoil.co.id>
2024-03-23 12:57:59 +0700

    Update Import

--- app/Imports/ScheduleImport.php
+++ app/Imports/ScheduleImport.php
@@ -12,21 +12,35 @@ use Carbon\Carbon;
 class ScheduleImport implements ToCollection, WithHeadingRow
 {
     /**
-    * @param Collection $collection
-    */
+     * @param Collection $collection
+     */
     public function collection(Collection $rows)
     {
-        
         foreach ($rows as $row) {
-            Schedule::create([
-                'schedule_code' => $row['schedule_code'],
-                'project' => $row['project'],
-                'employee' => $row['employee'],
-                'tanggal' => \Carbon\Carbon::parse($row['tanggal'])->toDateString(),
-                'shift' => $row['shift'],
-                'periode' => $row['periode'],
-            ]);
-           
+            // Periksa apakah jadwal dengan kode karyawan dan tanggal yang sama sudah ada
+            $existingSchedule = Schedule::where('employee', $row['employee'])
+                ->whereDate('tanggal', Carbon::parse($row['tanggal'])->toDateString())
+                ->first();
+                
+            if ($existingSchedule) {
+                // Jika sudah ada, perbarui catatan yang sudah ada
+                $existingSchedule->update([
+                    'schedule_code' => $row['schedule_code'],
+                    'project' => $row['project'],
+                    'shift' => $row['shift'],
+                    'periode' => $row['periode'],
+                ]);
+            } else {
+                // Jika belum ada, buat catatan baru
+                Schedule::create([
+                    'schedule_code' => $row['schedule_code'],
+                    'project' => $row['project'],
+                    'employee' => $row['employee'],
+                    'tanggal' => Carbon::parse($row['tanggal'])->toDateString(),
+                    'shift' => $row['shift'],
+                    'periode' => $row['periode'],
+                ]);
+            }
         }
     }
 }

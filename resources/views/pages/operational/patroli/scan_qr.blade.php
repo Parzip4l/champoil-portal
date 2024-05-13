4dc842d07f2e3dd59a6c384faa05ca8cf61a8774
yogiramdani <yogiramdani666@gmail.com>
2024-05-13 13:19:08 +0700

    ok

--- resources/views/pages/operational/patroli/scan_qr.blade.php
+++ resources/views/pages/operational/patroli/scan_qr.blade.php
@@ -47,7 +47,7 @@
                 "X-CSRF-Token": '{{ csrf_token() }}'
             },
             success: function(response) {
-                window.location.href = "https://hris.truest.co.id/checklist/"+decodedText;
+                window.location.href = "https://staging.truest.co.id/checklist/"+decodedText;
             }
         });
         

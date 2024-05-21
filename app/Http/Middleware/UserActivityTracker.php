ffc32e5f7b3d1dbf24f6278dec9266f3fb28442d
sobirinjamo33 <sobirinjamo33@gmail.com>
2024-05-21 15:10:32 +0700

    Update Activities Logs

--- app/Http/Middleware/UserActivityTracker.php
+++ app/Http/Middleware/UserActivityTracker.php
@@ -18,26 +18,58 @@ class UserActivityTracker
      * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
+    protected $excludedUrls = [
+        'activities', // URL yang akan dikecualikan dari pencatatan aktivitas
+    ];
+
     public function handle(Request $request, Closure $next)
     {
+        if (!$this->isExcludedUrl($request->url())) {
+            // Lakukan sesuatu sebelum request dijalankan
+            $requestedUrl = $request->fullUrl();
 
-        Log::info('Middleware UserActivityTracker sedang berjalan');
-        $response = $next($request);
+            // Panggil controller yang dijalankan
+            $response = $next($request);
 
-        // Lakukan sesuatu setelah request selesai dijalankan
-
-        // Cek apakah pengguna sedang login
-        if (Auth::check()) {
-            $code = Auth::user()->employee_code;
-            $company = Employee::where('nik', $code)->first();
-            // Simpan log aktivitas pengguna
-            $log = new ActivityLog();
-            $log->user_id = $code;
-            $log->action = 'Accessed: ' . $request->fullUrl();
-            $log->description = $company->unit_bisnis;
-            $log->save();
+            // Lakukan sesuatu setelah request selesai dijalankan
+            // Cek apakah pengguna sedang login
+            if (Auth::check()) {
+                $code = Auth::user()->employee_code;
+                $company = Employee::where('nik', $code)->first();
+                // Simpan log aktivitas pengguna
+                $log = new ActivityLog();
+                $log->user_id = $code;
+                $log->action = 'URL: ' . $requestedUrl . ' - Controller executed: ' . $this->getControllerName($response->original);
+                $log->description = $company->unit_bisnis;
+                $log->save();
+            }
+        } else {
+            // Jika URL termasuk dalam daftar URL yang dikecualikan, lanjutkan ke controller tanpa melakukan logging
+            $response = $next($request);
         }
 
-        return $next($request);
+        return $response;
+    }
+
+    protected function isExcludedUrl($url)
+    {
+        foreach ($this->excludedUrls as $excludedUrl) {
+            if (strpos($url, $excludedUrl) !== false) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    protected function getControllerName($controller)
+    {
+        if (is_string($controller)) {
+            return $controller;
+        } elseif (is_object($controller)) {
+            return get_class($controller);
+        } else {
+            return 'Unknown';
+        }
     }
 }

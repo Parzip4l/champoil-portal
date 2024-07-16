ed2d116e2c8bfe5cf567442c6885ba5a037806a2
yogiramdani <yogiramdani666@gmail.com>
2024-07-16 09:40:00 +0700

    ok

--- resources/views/pages/report/absen/index.blade.php
+++ resources/views/pages/report/absen/index.blade.php
@@ -99,10 +99,7 @@
                                     $color="";
                                 }
 
-                                if($row->name == 'ESCALIGHTS PLUIT'){
-                                    $row->schedule = $row->schedule*2;
-                                    $row->persentase_absen = round(($row->schedule / $row->absen) * 100, 2);
-                                }
+                              
                             @endphp
                             <tr style="{{ $color }}">
                                 <td>{{ $no }}</td>

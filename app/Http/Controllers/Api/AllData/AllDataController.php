9320d304ad6e058b1dee80574bab5f16446b5e72
yogiramdani <yogiramdani666@gmail.com>
2024-07-30 01:16:17 +0700

    ok

--- app/Http/Controllers/Api/AllData/AllDataController.php
+++ app/Http/Controllers/Api/AllData/AllDataController.php
@@ -180,7 +180,7 @@ class AllDataController extends Controller
                                     
                                     // Example of pushing message to Slack
                                     // Uncomment the following line to actually send the message
-                                   echo  push_slack_message('https://hooks.slack.com/services/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',$text);
+                                   echo  push_slack_message('https://hooks.slack.com/services/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',$text);
                                     
                                     // For testing, output the message
                                     // echo $text;

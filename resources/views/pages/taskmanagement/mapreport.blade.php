d2cc934dbf0853341d993f5f3afb515ebcee2587
sobirinjamo33 <sobirinjamo33@gmail.com>
2024-08-05 17:03:26 +0700

    Update Maps Track

--- resources/views/pages/taskmanagement/mapreport.blade.php
+++ resources/views/pages/taskmanagement/mapreport.blade.php
@@ -5,25 +5,16 @@
   <link href="{{ asset('assets/plugins/datatables-net-bs5/dataTables.bootstrap5.css') }}" rel="stylesheet" />
   <link href="{{ asset('assets/plugins/sweetalert2/sweetalert2.min.css') }}" rel="stylesheet" />
   <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+  <link rel="stylesheet" href="https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.css" />
 @endpush
 @push('style')
 <style>
         #map {
-            height: 500px;
+            height: 600px;
         }
         .leaflet-div-icon {
             background: none;
             border: none;
-        }
-        .leaflet-div-icon-start i {
-            font-size: 24px;
-        }
-        .leaflet-marker-icon.leaflet-div-icon-start.leaflet-zoom-animated.leaflet-interactive {
-            color : #27ae60;
-        }
-
-        .leaflet-marker-icon.leaflet-div-icon-end.leaflet-zoom-animated.leaflet-interactive {
-            color : #c0392b;
         }
     </style>
 @endpush
@@ -38,6 +29,7 @@
   <script src="{{ asset('assets/plugins/datatables-net-bs5/dataTables.bootstrap5.js') }}"></script>
   <script src="{{ asset('assets/plugins/sweetalert2/sweetalert2.min.js') }}"></script>
   <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+  <script src="https://unpkg.com/leaflet-routing-machine@3.2.12/dist/leaflet-routing-machine.js"></script>
   
 @endpush
 
@@ -57,35 +49,52 @@
     var locationsJson = '<?php echo json_encode($records) ?>';
     var locations = JSON.parse(locationsJson);
 
-    // Define custom icons using Feather icons
+    // Define custom icons using images
     var startIcon = L.divIcon({
         className: 'leaflet-div-icon-start',
-        html: '<i data-feather="map-pin" class="start"></i>', // Feather icon for start
-        iconSize: [24, 24]
+        html: '<img src="<?php echo asset('iconmap.png'); ?>" class="start-icon" />', // Image for start
+        iconSize: [15, 15]
     });
 
     var endIcon = L.divIcon({
         className: 'leaflet-div-icon-end',
-        html: '<i data-feather="flag" class="end"></i>', // Feather icon for end
-        iconSize: [24, 24]
+        html: '<img src="<?php echo asset('iconmap.png'); ?>" class="end-icon" />', // Image for end
+        iconSize: [15, 15]
     });
 
     locations.forEach(function(location) {
         // Add start marker with popup showing start time
-        L.marker([location.lat_start, location.lng_start], { icon: startIcon })
+        var startMarker = L.marker([location.lat_start, location.lng_start], { icon: startIcon })
             .addTo(map)
             .bindPopup(location.popup_start); // Popup for start point
 
         // Add end marker with popup showing end time
-        L.marker([location.lat_end, location.lng_end], { icon: endIcon })
+        var endMarker = L.marker([location.lat_end, location.lng_end], { icon: endIcon })
             .addTo(map)
             .bindPopup(location.popup_end); // Popup for end point
 
-        // Add a line between start and end points
-        L.polyline([
-            [location.lat_start, location.lng_start],
-            [location.lat_end, location.lng_end]
-        ], { color: 'red', weight: 3 }).addTo(map); // Ensure color and weight are set
+        // Add a route between start and end points using Leaflet Routing Machine
+        var control = L.Routing.control({
+            waypoints: [
+                L.latLng(location.lat_start, location.lng_start),
+                L.latLng(location.lat_end, location.lng_end)
+            ],
+            createMarker: function() { return null; }, // Remove default markers
+            lineOptions: {
+                styles: [{ color: 'red', weight: 5 }]
+            },
+            show: false, // Hide the control UI
+            addWaypoints: false
+        }).addTo(map);
+
+        control.on('routesfound', function(e) {
+            var routes = e.routes;
+            var summary = routes[0].summary;
+
+            // Update end marker popup with distance information
+            var distance = (summary.totalDistance / 1000).toFixed(2); // Convert to km and format to 2 decimals
+            endMarker.bindPopup(location.popup_end + '<br>Jarak Tempuh: ' + distance + ' Km').openPopup();
+        });
     });
 </script>
 

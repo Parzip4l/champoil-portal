03c89949edb4637e2b9e6d53c76eaec50df5ade5
sobirinjamo33 <sobirinjamo33@gmail.com>
2024-09-23 22:43:24 +0700

    Update Loan Status

--- app/Http/Controllers/Api/KoperasiApi/AllKoperasiController.php
+++ app/Http/Controllers/Api/KoperasiApi/AllKoperasiController.php
@@ -9,6 +9,7 @@ use App\Koperasi\Anggota;
 use App\Koperasi\Loan;
 use App\Koperasi\Saving;
 use App\Koperasi\LoanPayment;
+use App\Koperasi\SettingLoan;
 use App\Employee;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
@@ -18,6 +19,7 @@ use Illuminate\Support\Facades\DB;
 use App\Slack;
 use App\ModelCG\Schedule;
 use App\Absen;
+use Illuminate\Support\Str;
 use App\Mail\Koperasi\PengajuanAnggota;
 use App\Mail\Koperasi\PengajuanAnggotaReject;
 use Illuminate\Support\Facades\Mail;
@@ -114,9 +116,6 @@ class AllKoperasiController extends Controller
             return response()->json([
                 'success' => true,
                 'message' => 'Loan data retrieved.',
-                'syarat1' => $isMemberForThreeMonths,
-                'syarat2' => $hasNoOutstandingLoan,
-                'syarat3' => $hadFullAttendance,
                 'status_anggota' => $anggotaStatus->member_status,
                 'onloan_status' => $loan,
                 'remaining_loan' => $pinjaman,
@@ -192,24 +191,55 @@ class AllKoperasiController extends Controller
         }
     }
 
-    public function dataSaving(Request $request)
+    public function pengajuanPinjaman(Request $request)
     {
-        try {
+        try 
+        {
             $token = $request->bearerToken();
             // Authenticate the user based on the token
             $user = Auth::guard('api')->user();
             $employeeCode = $user->name;
             $employeeData = Employee::where('nik', $employeeCode)->first();
 
-            // Buat instance model Anggota
-            $datasaya = Saving::where('employee_id',$employeeCode)->get();
-    
-            // Kembalikan response JSON sukses
+            $koperasi = Koperasi::where('company', $employeeData->unit_bisnis)->first();
+            $pinjamanAwal = $request->amount;
+            $tenor = $koperasi->tenor;
+            $membership = $koperasi->membership / 100;
+            $merchendise = $koperasi->merchendise / 100;
+            $persentase = $membership + $merchendise;
+            $kalkulasi = $pinjamanAwal * $persentase;
+            $totalPinjaman = $pinjamanAwal + $kalkulasi;
+
+            $instalment = round($totalPinjaman / $tenor);
+            $loanUuid = Str::uuid();
+            // Simpan data ke database
+            Loan::create([
+                'id' => $loanUuid,
+                'company' => $employeeData->unit_bisnis,
+                'nama' => $employeeData->nama,
+                'employee_code' => $employeeCode,
+                'amount' => $totalPinjaman,
+                'tenor' => $tenor,
+                'instalment' => $instalment,
+                'status' => 'waiting',
+                'approve_by' => '-',
+            ]);
+
+            Anggota::where('employee_code', $employeeCode)->update(['loan_status' => 'onloan']);
+            $today = Carbon::now()->format('Y-m-d');
+            LoanPayment::create([
+                'loan_id' => $loanUuid,
+                'tanggal_pembayaran' => $today,
+                'jumlah_pembayaran' => 0,
+                'sisahutang' => $totalPinjaman,
+            ]);
+
             return response()->json([
                 'success' => true,
-                'data' => $datasaya
+                'message' => 'Thank you for registering, we are reviewing your data first.'
             ], 201);
-    
+
+
         } catch (\Exception $e) {
             // Tangani pengecualian di sini dan kembalikan response JSON error
             return response()->json([
@@ -219,7 +249,7 @@ class AllKoperasiController extends Controller
         }
     }
 
-    public function dataPinjaman(Request $request)
+    public function cekLimit(Request $request)
     {
         try {
             $token = $request->bearerToken();
@@ -227,14 +257,32 @@ class AllKoperasiController extends Controller
             $user = Auth::guard('api')->user();
             $employeeCode = $user->name;
             $employeeData = Employee::where('nik', $employeeCode)->first();
-
+            $unitBisnis = Employee::where('nik', $employeeCode)->value('unit_bisnis');
+            $koperasi = Koperasi::where('company', $unitBisnis)->select('membership','merchendise','tenor')->get();
             // Buat instance model Anggota
             $datasaya = Saving::where('employee_id',$employeeCode)->get();
+            $dataSaya = Anggota::where('employee_code',$employeeCode)
+                            ->select('saldosimpanan')->first();
+
+            if (!$dataSaya) {
+                abort(404, 'Data anggota tidak ditemukan.');
+            }
+            
+            $saldosimpanan = $dataSaya->saldosimpanan;
+            $loansettings = SettingLoan::where('company',$employeeData->unit_bisnis)->get();
+
+            foreach ($loansettings as $datapinjaman) {
+                if ($saldosimpanan >= $datapinjaman->min_saving && $saldosimpanan <= $datapinjaman->max_saving) {
+                    $limitpinjaman = $datapinjaman->max_limit;
+                    break;
+                }
+            }
     
             // Kembalikan response JSON sukses
             return response()->json([
                 'success' => true,
-                'data' => $datasaya
+                'limit_saya' => $limitpinjaman,
+                'koperasi' => $koperasi,
             ], 201);
     
         } catch (\Exception $e) {

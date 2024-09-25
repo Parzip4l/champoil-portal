970df5a78386e7c47b2999d5a8cae8c23d09fc58
Yogi Ramdani <yogiramdani666@gmail.com>
2024-09-25 17:05:59 +0700

    ok

--- app/Http/Controllers/Api/Schedular/DailyContrtoller.php
+++ app/Http/Controllers/Api/Schedular/DailyContrtoller.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api\Schedular;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\ModelCG\Project;
 use App\ModelCG\Schedule;
+use App\ModelCG\Absen;
 use Carbon\Carbon;
 
 class DailyContrtoller extends Controller
@@ -14,47 +16,52 @@ class DailyContrtoller extends Controller
         $records = Project::whereNull('deleted_at')->get();
         $yesterday = Carbon::yesterday()->format('Y-m-d');
         $result = [];
-
+        
         if (!$records->isEmpty()) {
             foreach ($records as $row) {
                 // Fetch all schedules for this project on the given day where shift is not 'OFF'
                 $schedules = Schedule::where('schedules.project', $row->id)
                     ->where('shift', '!=', 'OFF')
                     ->where('schedules.tanggal', $yesterday)
-                    ->where('absens.tanggal', $yesterday)
-                    ->leftJoin('absens', 'absens.nik', '=', 'schedules.employee')
                     ->get();
-
+        
                 // Total number of schedules
                 $schedules_total = $schedules->count();
-
+        
                 // Initialize counters
                 $absen = 0;
                 $not_absen = 0;
-
+        
                 // Count absentees and presentees based on clock_in field
                 foreach ($schedules as $rs) {
-                    if ($rs->status == 'H') {
+                    // Check attendance for the employee on the given date
+                    $jml_absen = DB::table('absens')
+                        ->where('nik', $rs->employee)
+                        ->where('tanggal', $yesterday)
+                        ->count();
+
+                    if ($jml_absen > 0) {
                         $absen += 1;
                     } else {
                         $not_absen += 1;
                     }
                 }
-
+        
                 // Add result for each project
                 $result[] = [
                     "project_name" => $row->name,
-                    "schedule_on" => $schedules_total,
-                    "absen" => $absen,
-                    "not_absen" => $not_absen
+                    "schedule_on" => $schedules_total,  
+                    "absen" => $absen,                 
+                    "not_absen" => $not_absen           
                 ];
             }
         }
-
+        
         // Return the records as a JSON response
         return response()->json([
             'status' => 'success',
             'data' => $result
         ]);
+        
     }
 }

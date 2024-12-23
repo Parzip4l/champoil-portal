ab343002657b662bc79ed9ba47e5f13b7a4e4132
Yogi Ramdani <yogiramdani666@gmail.com>
2024-12-23 15:37:37 +0700

    report patroli

--- app/Http/Controllers/Recruitments/PenempatanController.php
+++ app/Http/Controllers/Recruitments/PenempatanController.php
@@ -40,7 +40,7 @@ class PenempatanController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index() 
     {
         
         $result=[];

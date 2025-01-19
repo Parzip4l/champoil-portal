8c9508794f4c62613767bb9c6add6083580e2bb6
Yogi Ramdani <yogiramdani666@gmail.com>
2025-01-19 15:46:28 +0700

    pppaklaring

--- resources/views/pages/hc/karyawan_resign/paklaring.blade.php
+++ resources/views/pages/hc/karyawan_resign/paklaring.blade.php
@@ -76,22 +76,25 @@
             <td>PT Kharisma Adhi Sejahtera</td>
         </tr>
     </table>
-    
     <p>
         Kami mengucapkan banyak terima kasih atas jasa dan kerjasama yang saudara/i berikan ke PT Kharisma Adhi Sejahtera, 
         dan semoga keberhasilan senantiasa menyertai Anda di masa yang akan datang.<br><br>
         We would like to take this opportunity to thank you for your sincere efforts and contribution to PT Kharisma Adhi Sejahtera, 
         and we wish you every success in the future.
     </p>
-    <table style="width: 100%; margin-bottom: 40px; text-align: center;font-size:20px;">
-        <tr>
-            <td>
-                Jakarta,{{date('d F Y')}}<br/>
-                Hormat Saya
-                <img src="https://hris.truest.co.id/images/company_logo/ttd.png" style="width: 200px;" alt="Company Logo">
-                <img src="https://hris.truest.co.id/images/company_logo/stampel.png" style="width: 200px;" alt="Company Logo">
-            </td>
-        </tr>
-    </table>
+    <table style="width: 100%; margin-bottom: 10px; text-align: left; font-size: 20px;">
+    <tr>
+        <td style="border: 1px solid black;">
+            Jakarta, {{ date('d F Y') }}<br/>
+            Hormat Saya<br/>
+            <img src="https://hris.truest.co.id/images/company_logo/ttd.png" style="width: 100px; text-align: left;" alt="Company Logo">
+            <img src="https://hris.truest.co.id/images/company_logo/stampel.png" style="width: 200px;margin-top:20px;margin-left:-60px;" alt="Company Logo"><br/>
+            <u><strong>Moch. Firly Triyodha Kusuma, SE</strong></u><br/>
+            <p>Human Culture Manager </p>
+
+        </td>
+    </tr>
+</table>
+
 </body>
 </html>

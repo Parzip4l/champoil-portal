962fe0a3c1520c5b0f55f58a7d1c559ed0f79502
Yogi Ramdani <yogiramdani666@gmail.com>
2025-02-12 15:44:27 +0700

    reset password

--- resources/views/emails/forgot-password.blade.php
+++ resources/views/emails/forgot-password.blade.php
@@ -2,16 +2,16 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Welcome to the Company</title>
+    <title>Welcome to TRUEST</title>
 </head>
 <body>
 
     <p><strong>Set Up Your Password:</strong></p>
     <p>To get started, please set up your password by clicking the link below:</p>
-    <p><a href="{{ url('/form-forgot-password/' . $resetLink) }}" style="color: #007bff; text-decoration: none; font-weight: bold;">Reset Your Password</a></p>
+    <p><a href="{{ $resetLink }}" style="color: #007bff; text-decoration: none; font-weight: bold;">Reset Your Password</a></p>
 
     <p>If the button above does not work, copy and paste this URL into your browser:</p>
-    <p><strong>{{ url('/form-forgot-password/' . $resetLink) }}</strong></p>
+    <p><strong>{{ $resetLink }}</strong></p>
 
     <p>If you have any questions or need further assistance, feel free to reach out to the HR department.</p>
 

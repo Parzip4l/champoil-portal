e59b9929e1ba6ee285389edf0bcef1efa49943c6
Yogi Ramdani <yogiramdani666@gmail.com>
2025-02-17 09:09:13 +0700

    koperasi

--- resources/views/pages/report/absen/index.blade.php
+++ resources/views/pages/report/absen/index.blade.php
@@ -221,10 +221,10 @@
 <div class="modal fade" id="exportPayrollModal" tabindex="-1" aria-labelledby="exportPayrollModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
-            <div class="modal-header">
+            <!-- <div class="modal-header">
                 <h5 class="modal-title" id="exportPayrollModalLabel">Export Absen</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
+            </div> -->
             <div class="modal-body">
                 <form action="" method="GET">
                     <div class="form-group">

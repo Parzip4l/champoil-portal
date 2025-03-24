fec23e7bd1223615cf08a1e60121a96ef29131bf
sobirinjamo33 <sobirinjamo33@gmail.com>
2025-03-25 01:11:51 +0700

    Update logs apps

--- app/Helpers/LogHelper.php
+++ app/Helpers/LogHelper.php
@@ -9,6 +9,7 @@ class LogHelper
 {
     public static function storeLog($action, $description = null)
     {
+        $user = Auth::guard('api')->user(); // Ambil user dari guard API
         $log = new Log();
         $log->user_id = Auth::check() ? Auth::id() : null; // ID user jika ada
         $log->action = $action;

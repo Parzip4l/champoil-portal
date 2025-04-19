c4c2c837b3ec066eda511951952610b6811756c9
parzip4l <sobirinjamo33@gmail.com>
2025-04-19 18:27:20 +0700

    Perbaikan Posisi

--- resources/views/pages/app-setting/index.blade.php
+++ resources/views/pages/app-setting/index.blade.php
@@ -7,19 +7,19 @@
 @endpush
 
 @section('content')
+@if(session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif  
+
+@if (session('error'))
+    <div class="alert alert-danger">
+        {{ session('error') }}
+    </div>
+@endif
 <div class="row">
     <div class="col-md-12 grid-margin stretch-card">
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif  
-
-        @if (session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
         <div class="card custom-card2">
             <div class="card-header">
                 <div class="row">

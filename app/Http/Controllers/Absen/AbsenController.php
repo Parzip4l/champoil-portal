e1ebd0d13ec71ba64dec410a3a65418462155576
Yogi Ramdani <yogiramdani666@gmail.com>
2025-04-28 12:20:53 +0700

    attendadnce records

--- app/Http/Controllers/Absen/AbsenController.php
+++ app/Http/Controllers/Absen/AbsenController.php
@@ -131,6 +131,7 @@ class AbsenController extends Controller
 
         $absens = Absen::whereBetween('tanggal', [reset($dates), end($dates)])
             ->whereIn('nik', $niks)
+            ->whereNull('project_backup')
             ->get()
             ->groupBy('nik');
         

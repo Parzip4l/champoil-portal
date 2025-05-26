ccc912334627549506f98df78a4acf8e0f00c659
Yogi Ramdani <yogiramdani666@gmail.com>
2025-05-26 14:05:26 +0700

    ok

--- app/Http/Controllers/Api/ProfileController.php
+++ app/Http/Controllers/Api/ProfileController.php
@@ -213,16 +213,16 @@ class ProfileController extends Controller
         }catch (ValidationException $exception) {
             DB::rollBack();
             $errorMessage = $exception->validator->errors()->first(); // ambil pesan error pertama dari validator
-            // if (!$employee->save()) {
-            //     $msg='Gagal menyimpan data karyawan.';
-            //     $error=true;
-            // }
-            $error=true;
-            // if (!$payrollInfo->save()) {
-            //     return redirect()->back()->with('error', '' . $errorMessage);
-            //     $msg='Gagal menyimpan data Payroll Info.';
-            //     $error=true;
-            // }
+            if (!$employee->save()) {
+                $msg='Gagal menyimpan data karyawan.';
+                $error=true;
+            }
+
+            if (!$payrollInfo->save()) {
+                return redirect()->back()->with('error', '' . $errorMessage);
+                $msg='Gagal menyimpan data Payroll Info.';
+                $error=true;
+            }
         }
 
         $result=[
